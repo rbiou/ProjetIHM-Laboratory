16a7f402e0a4db4a40a88ffadd69bef1c41cda4b
cfoucher49 <45234511+cfoucher49@users.noreply.github.com>
2019-04-12 19:53:52 +0200

    Update ProjetIHM.java

--- src/projetihm/ProjetIHM.java
+++ src/projetihm/ProjetIHM.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projetihm;
 
 import javafx.application.Application;
@@ -12,17 +7,20 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 /**
+ * Cette classe permet de lancer notre scene contenant tous les éléments définis
+ * dans le document FXML
  *
- * @author Remi
+ * @version 12/04/19
+ * @author Biou Rémi, Foucher Chloé
  */
 public class ProjetIHM extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+
         Scene scene = new Scene(root);
-        
+
         stage.setScene(scene);
         stage.show();
     }
@@ -33,5 +31,5 @@ public class ProjetIHM extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
